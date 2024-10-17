d503cee4fdb215077b04252114c3d6f97499f518
Mehmet Emre <memre@usfca.edu>
2024-10-16 19:14:31 -0700

    fix formatting and make meerkat happy

--- src/main/scala/MeerkatJSONParser.scala
+++ src/main/scala/MeerkatJSONParser.scala
@@ -1,6 +1,5 @@
-
 import org.meerkat.Syntax._
-import org.meerkat.parsers._
+import org.meerkat.parsers.{Seq => _, _}
 import Parsers._
 
 //class MeerkatJSONParser{
@@ -50,62 +49,83 @@ import Parsers._
 //  }
 //}
 
-
-
 class MeerkatJSONParser {
 
   // JSONNumber AST nodes
-  val num: Nonterminal & JSONNumber = syn("[0-9]+".r) & ((s: String) => JSONNumber(s.toDouble))
-  //val num: Nonterminal & JSONNumber = syn("[0-9]+".r) ^ ((s: String) => JSONNumber(s.toDouble))\
+  val num: Nonterminal & JSONNumber = syn(
+    "[0-9]+".r ^ ((s: String) => JSONNumber(s.toDouble))
+  )
+  // val num: Nonterminal & JSONNumber = syn("[0-9]+".r) ^ ((s: String) => JSONNumber(s.toDouble))\
 
   // JSONString AST nodes
-  val str: Nonterminal & JSONString = syn("\"[^\"]*\"".r) & ((s: String) => JSONString(s.substring(1, s.length - 1)))
+  val str: Nonterminal & JSONString = syn(
+    "\"[^\"]*\"".r ^ ((s: String) => JSONString(s.substring(1, s.length - 1)))
+  )
 
   // JSONNull AST node
-  val `null`: Nonterminal & JSONNull.type = syn("null") & (_ => JSONNull)
+  val `null`: Nonterminal & JSONNull.type = syn("null" & (_ => JSONNull))
 
   // JSONBoolean AST nodes
-  val bool: Nonterminal & JSONBoolean = syn("true" | "false") & ((s: String) => JSONBoolean(s.toBoolean))
+  val bool: Nonterminal & JSONBoolean = syn(
+    "true" & (_ => JSONBoolean(true))
+    | "false" & (_ => JSONBoolean(false))
+  )
 
   // JSON objects
-  val keyPair: Nonterminal & (String, JSONValue) = syn { str ~ ":" ~ value } & { case key ~ _ ~ v => key.value -> v }
+  val keyPair: Nonterminal & (String, JSONValue) = syn {
+    (str ~ ":" ~ value) & { case key ~ v => key.value -> v 
+    } | "$" & (_ => ???)  // This is a hack to make Meerkat macros work
+  }
 
   // JSONObject AST nodes
-  val obj: Nonterminal & JSONObject = syn { "{" ~ maybe(keyPair ~ keyPairs) ~ "}" } & {
-    case _ ~ Some(firstPair ~ restPairs) ~ _ => JSONObject((Seq(firstPair) ++ restPairs).toMap)
-    case _ ~ None ~ _                        => JSONObject(Map.empty)
+  val obj: Nonterminal & JSONObject = syn {
+    ("{" ~ maybe(keyPair ~ keyPairs) ~ "}") & {
+      case Some(firstPair ~ restPairs) =>
+        JSONObject((firstPair +: restPairs).toMap)
+      case None => JSONObject(Map.empty)
+    } | "$" & (_ => ???)  // This is a hack to make Meerkat macros work
   }
 
   // JSONArray AST nodes
-  val arr: Nonterminal & JSONArray = syn { "[" ~ maybe(value ~ values) ~ "]" } & {
-    case _ ~ Some(firstValue ~ restValues) ~ _ => JSONArray(firstValue +: restValues)
-    case _ ~ None ~ _                          => JSONArray(Seq.empty)
+  val arr: Nonterminal & JSONArray = syn {
+    ("[" ~ maybe(value ~ values) ~ "]") & {
+      case Some(firstValue ~ restValues) =>
+        JSONArray(firstValue +: restValues)
+      case None => JSONArray(Seq.empty)
+    } | "$" & (_ => ???)  // This is a hack to make Meerkat macros work
   }
   // Values: objects, arrays, strings, numbers, booleans, or null
-  val value: Nonterminal & JSONValue = syn { obj | arr | str | num | bool | `null` }
+  val value: Nonterminal & JSONValue = syn {
+    obj | arr | str | num | bool | `null`
+  }
 
-  //csv in arrays
-  def values: Nonterminal & Seq[JSONValue] = syn { "," ~ value ~ values | epsilon } & {
-    case _ ~ v ~ vs => v +: vs
-    case _          => Seq.empty
+  // csv in arrays
+  def values: Nonterminal & Seq[JSONValue] = syn {
+    ("," ~ value ~ values & { case v ~ vs => v +: vs }) | (epsilon & { case _ => Seq.empty })
   }
 
   // csv pairs in objects
-  def keyPairs: Nonterminal & Seq[(String, JSONValue)] = syn { "," ~ keyPair ~ keyPairs | epsilon } & {
-    case _ ~ kp ~ kps => kp +: kps
-    case _            => Seq.empty
+  val keyPairs: Nonterminal & Seq[(String, JSONValue)] = syn {
+    ("," ~ keyPair ~ keyPairs & { case kp ~ kps => kp +: kps }) | (epsilon & { case _ =>
+      Seq.empty
+    })
   }
 
-  def maybe(p: SequenceBuilder[NoValue]): Nonterminal = syn { p | epsilon }
+  def maybe[A](p: SequenceBuilder[A]): Nonterminal & Option[A] = syn { 
+    p & { case v => Some(v) } | epsilon & { case _ => None }
+   }
 
   def parse(input: String): Either[String, JSONValue] = {
     exec(value, input) match {
       case Left(error) =>
         println(s"Meerkat parse error: $error") // Log the error
-        Left(s"Parse error: $error")            // Return the error as a Left
+        Left(s"Parse error: $error") // Return the error as a Left
       case Right(result) =>
         println(s"Meerkat parse succeeded: $result") // Log the success
-        Right(result.asInstanceOf[ParseSuccess].value) // Return the successful result as a Right
+        Right(
+          //result.asInstanceOf[ParseSuccess].value
+          ???
+        ) // Return the successful result as a Right
     }
   }
 }
